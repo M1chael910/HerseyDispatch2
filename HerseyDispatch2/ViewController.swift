9e4197663dde97eb78763ba4b8a61a752958b3de
Michael Murphy <mpmurphy1432@gmail.com>
2019-04-05 20:56:11 -0500

    Initial Commit

---
+++ HerseyDispatch2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HerseyDispatch2
+//
+//  Created by Michael  Murphy on 4/5/19.
+//  Copyright © 2019 Michael  Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
