68a9a49099d4fbc844d2c54954fdc56f303bb3f9
Alexander Nowak <arnowak03@gmail.com>
2019-05-14 14:11:31 -0500

    fixedDaily

--- HerseyDispatch2/Controller/DailyViewController.swift
+++ HerseyDispatch2/Controller/DailyViewController.swift
@@ -10,39 +10,23 @@ import UIKit
 import WebKit
 import Firebase
 import GoogleSignIn
+import SafariServices
+class DailyViewController: UIViewController {
 
-class DailyViewController: UIViewController, WKNavigationDelegate {
-    @IBOutlet weak var dailyWebView: WKWebView!
     @IBOutlet weak var loadingView: UIImageView!
     var dailyURL = URL(string: "https://drive.google.com/file/d/1GUAAtEA769ZH3Z5vn-azvag79yvOUzSQ/view?usp=sharing")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let dailyRequest = URLRequest(url: dailyURL!)
-        dailyWebView.load(dailyRequest)
         loadingView.alpha = 1
-        dailyWebView.navigationDelegate = self
     }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        loadingView.alpha = 0
-    }
-    
-    
     override func viewDidAppear(_ animated: Bool) {
-        Analytics.logEvent("daily_pressed", parameters: nil)
-
+        let config = SFSafariViewController.Configuration()
+        let vc = SFSafariViewController(url: dailyURL!, configuration: config)
+        present(vc, animated: true)
     }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+
+
     
 }
 
